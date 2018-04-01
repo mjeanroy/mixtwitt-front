9f432355e5db6ecaa87ffa6db73208b513a4786a
Houssem BELHADJ AHMED <houssem@belhadjahmed.com>
2018-04-01 12:01:00 +0200

    hardcode service endpoint

--- src/js/app.js
+++ src/js/app.js
@@ -7,7 +7,6 @@ import {routingModule} from './routing/index';
 import {securityModule} from './security/index';
 import {loginModule} from './login/index';
 import {tweetModule} from './tweet/index';
-
 angular
   .module('mixtwitt', [routingModule.name, securityModule.name, loginModule.name, tweetModule.name])
-  .value('API', 'http://localhost:8080');
+  .value('API', 'https://service-app.mr-robot.sh');
